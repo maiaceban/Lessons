f141b50eb7b64543f2a8478679b4746072d0304c
Maia <maya.ceb.96@gmail.com>
2022-04-28 20:07:58 +0300

    Commit .gitignore

---
+++ Exercitii ore/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = -127;
+        short shortNum = b;
+
+        int intNum = 235;
+        long longNum = intNum;
+
+        float floatNum = 122356F;
+        double doubleNum = floatNum;
+
+
+        System.out.println(shortNum+longNum+doubleNum);
+
+       
+
+
+    }
+}
