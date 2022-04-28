d10d2b1f70804a06603ecb8b78caf0e43048fa81
Maia <maya.ceb.96@gmail.com>
2022-04-28 21:26:14 +0300

    Commit Ex 3

---
+++ Lesson 2/Class Exercise 3.1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ten = 10;
+        int oneHundred = 100;
+        int twelve = 12;
+        int eighty = 80;
+        int five = 5;
+        System.out.println(ten + " " + oneHundred + " " + twelve + " " + eighty + " " + five);
+    }
+}
