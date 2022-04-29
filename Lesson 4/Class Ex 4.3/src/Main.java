85f0be46fbac92ee3cdda1f8ff33f1b3a011d660
Maia <maya.ceb.96@gmail.com>
2022-04-29 20:38:11 +0300

    Commit ex

--- Lesson 4/Class Ex 4.3/src/Main.java
+++ Lesson 4/Class Ex 4.3/src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("What is the value of x?: ");
+        double x = scr.nextDouble();
+
+        System.out.println(Math.pow(x, 3) + Math.pow(x, 2) + x + 1);
+
+    }
 }
