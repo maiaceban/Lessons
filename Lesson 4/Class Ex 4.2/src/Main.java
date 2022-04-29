6925aa29ec9c0a23acdab0aae92105af7b7246a4
Maia <maya.ceb.96@gmail.com>
2022-04-29 20:36:24 +0300

    Commit

--- Lesson 4/Class Ex 4.2/src/Main.java
+++ Lesson 4/Class Ex 4.2/src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("What is the value of n?: ");
+        int n = scr.nextInt();
+
+        int result = ((n + 1) * n + 2) * n + 3;
+        System.out.println(result);
+    }
 }
