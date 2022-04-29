a791607e05816ba3f9d55f3b47df6054bd5f0e07
Maia <maya.ceb.96@gmail.com>
2022-04-29 20:39:36 +0300

    Commit ex

--- Lesson 4/Class Ex 4.4/src/Main.java
+++ Lesson 4/Class Ex 4.4/src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a + b == 20 || a + c == 20 || b + c == 20);
+    }
 }
+
