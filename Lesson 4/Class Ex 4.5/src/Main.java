a8f50ef155ba1bf9046662a8a02db83dceb53251
Maia <maya.ceb.96@gmail.com>
2022-04-30 12:41:20 +0300

    Commit class Ex4.5

---
+++ Lesson 4/Class Ex 4.5/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/* Write a program that finds the number of tens in a non-negative integer N(0<N<100000).
+ If N is 264, the number of tens is 6
+ If N is 4136, the number of tens is 3
+ If N is 101, the number of tens is 0 */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println((num % 100) / 10);
+
+    }
+}
