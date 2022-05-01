edbd327ac28b944e946c3020b6f2a569cc8507bf
Maia <maya.ceb.96@gmail.com>
2022-05-01 12:53:57 +0300

    HOMEWORK Ex 4.1

---
+++ Lesson 4/Exercise 4.1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        System.out.println("Output numbers:" + '\n' + a + '\n' + b + '\n' + c +'\n' + d );
+    }
+}
