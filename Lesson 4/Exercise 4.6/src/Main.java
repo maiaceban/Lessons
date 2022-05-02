5a0daab57e71fc3a564946c515b89a4d06a0ce2c
Maia <maya.ceb.96@gmail.com>
2022-05-02 16:50:06 +0300

    HOMEWORK Ex 4.6

--- Lesson 4/Exercise 4.6/src/Main.java
+++ Lesson 4/Exercise 4.6/src/Main.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
         double a = 18.4;
         double b = -5.0;
         System.out.println((b-a));
-
     }
 }
