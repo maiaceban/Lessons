bb9a72ec226869dd47304f8bec3f907e47fea153
Maia <maya.ceb.96@gmail.com>
2022-05-02 15:06:52 +0300

    HOMEWORK Exb 4.3

--- Lesson 4/Exercise 4.3/src/Main.java
+++ Lesson 4/Exercise 4.3/src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int number = 476;
+        int sum = 0;
+        int num = number;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            sum += lastDigit;
+            num /= 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
 }
