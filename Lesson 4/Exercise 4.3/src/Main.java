8e1d549a6c85bf0a8e823de9ae104cf6ac588ce3
Maia <maya.ceb.96@gmail.com>
2022-05-04 19:10:04 +0300

    HOMEWORK

--- Lesson 4/Exercise 4.3/src/Main.java
+++ Lesson 4/Exercise 4.3/src/Main.java
@@ -1,13 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int number = 476;
-        int sum = 0;
-        int num = number;
-        while (num > 0) {
-            int lastDigit = num % 10;
-            sum += lastDigit;
-            num /= 10;
-        }
-        System.out.println("Sum of digits: " + sum);
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();  // i = 476
+
+        int i1 = i / 100;  // 4
+        int i2 = (i % 100) / 10;
+        int i3 = i % 10;
+        int resulte = i1 + i2 + i3;
+        System.out.println(resulte);
     }
 }
