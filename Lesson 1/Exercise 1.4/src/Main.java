638510d02b0b2a1973ec38dbce7da12cf069efdc
Maia <maya.ceb.96@gmail.com>
2022-05-04 20:36:28 +0300

    Commit Lesson 1

--- Lesson 1/Exercise 1.4/src/Main.java
+++ Lesson 1/Exercise 1.4/src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WE NEED TO");
+        System.out.println();
+        System.out.println("LEARN JAVA");
+        System.out.println();
+        System.out.println("AS QUICKLY AS POSSIBLE");
+    }
 }
--- Lesson 1/Exercise 1.5/src/Main.java
+++ Lesson 1/Exercise 1.5/src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("O  X  X");
+        System.out.println("O  X  O");
+        System.out.println("X  O  X");
+    }
 }
