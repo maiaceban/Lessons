22a346065804e6ac8aa56e1282c8685db8ddd2c9
Maia <maya.ceb.96@gmail.com>
2022-05-16 13:54:58 +0300

    commit

--- Lesson 5/HOMEWORK/src/lesson5class/exercise2/Main.java
+++ Lesson 5/HOMEWORK/src/lesson5class/exercise2/Main.java
@@ -1,2 +1,12 @@
-package lesson5class.exercise2;public class Main {
+package lesson5class.exercise2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        String result = number > 0 ? "YES" : "NO";
+        System.out.println(result);
+    }
 }
