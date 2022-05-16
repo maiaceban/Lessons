92ef783e99738fdb4de9d126599beda7991e73d2
Maia <maya.ceb.96@gmail.com>
2022-05-16 20:31:00 +0300

    Class Ex 4.2

---
+++ Lesson 4/Class Exercise 4.2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("What is the value of n?: ");
+        int n = scr.nextInt();
+
+
+        int result = ((n + 1) * n + 2) * n + 3;
+        System.out.println(result);
+
+    }
+}
