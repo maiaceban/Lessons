893c96e46f129daee519f8f20aeb7fbc9310eb8b
Maia <maya.ceb.96@gmail.com>
2022-05-17 12:14:04 +0300

    Minesweeper

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("How many mines do you want on the field? ");
+        int a = scanner.nextInt();
+
+        System.out.println(".x.......");
+        System.out.println(".....x...");
+        System.out.println(".x.......");
+        System.out.println("...x.....");
+        System.out.println("......x..");
+        System.out.println(".x.......");
+        System.out.println("....x....");
+        System.out.println(".x.......");
+        System.out.println(".....x...");
+    }
+}
