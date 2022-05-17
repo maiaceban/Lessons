baf6f2c106682b61a9dbe07cde0dbf24ab16d551
Maia <maya.ceb.96@gmail.com>
2022-05-17 11:51:14 +0300

    class ex

---
+++ Lesson 5/Class Exercise 5.10/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long nr1 = scanner.nextLong();
+        String op = scanner.next();
+        long nr2 = scanner.nextLong();
+
+        switch (op) {
+            case "+" -> System.out.println(nr1 + nr2);
+            case "-" -> System.out.println(nr1 - nr2);
+            case "/" -> {
+                if (nr2 != 0) {
+                    System.out.println(nr1 / nr2);
+                } else {
+                    System.out.println("Division by 0!");
+                }
+            }
+            case "*" -> System.out.println(nr1 * nr2);
+            default -> System.out.println("Unknown operator");
+        }
+    }
+}
