b76699e8512906aa7968eedd2fc8ce558e412c5d
Maia <maya.ceb.96@gmail.com>
2022-05-17 16:20:23 +0300

    class ex

--- Lesson 6/Class Exercise 6.2/src/Main.java
+++ Lesson 6/Class Exercise 6.2/src/Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] arrSise = new int[5];
+        int arrSize = scanner.nextInt();
+        int[] arr = new int[arrSize];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int value : arr) {
+            sum += value;
+        }
+        System.out.println(sum);
     }
 }
