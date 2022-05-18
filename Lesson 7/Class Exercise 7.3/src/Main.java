33242de6083ed2c5075c726dff9fad9d27a26458
Maia <maya.ceb.96@gmail.com>
2022-05-18 19:12:27 +0300

    class exercise 7.3

---
+++ Lesson 7/Class Exercise 7.3/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine(); //one two three four five six
+
+        String[] words = line.split(" ");
+
+        String result = words[0]; //one
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i]; //two
+            if (result.length() < word.length()) {
+                result = word;
+            }
+        }
+        System.out.println(result);
+    }
+
+}
+
