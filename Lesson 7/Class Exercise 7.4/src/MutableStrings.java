c8edb45ee55c176290f94a33ee8e91cfadddcf88
Maia <maya.ceb.96@gmail.com>
2022-05-18 21:00:32 +0300

    Class exercise

--- Lesson 7/Class Exercise 7.4/src/MutableStrings.java
+++ Lesson 7/Class Exercise 7.4/src/MutableStrings.java
@@ -5,7 +5,7 @@ public class MutableStrings {
         Scanner scanner = new Scanner(System.in);
         String fullName = scanner.nextLine();
 
-        StringBuilder name = new StringBuilder("Juc Gabriel");
+        StringBuilder name = new StringBuilder(fullName); // Juc Gabriel
         name.delete(1, 3);
         name.delete(3, 10);
         System.out.println(name);
