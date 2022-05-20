efafa315c1382d94b424f025c0e4b03baa2fe581
Maia <maya.ceb.96@gmail.com>
2022-05-20 18:34:52 +0300

    8.Concat all strings without digits

---
+++ Lesson 7/Exercise 7.8/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*/
+        Write a program that reads a string with multiple words, and concatenate all words
+        to a single long string. You must skip all digits inside the input strings.
+        Use StringBuilder to solve the problem.
+        Sample Input: T7est i1nput
+        Sample Output: Testinput
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        StringBuilder str = new StringBuilder(string); // T7est i1nput
+        str.deleteCharAt(1);
+        str.deleteCharAt(4);
+        str.deleteCharAt(5);
+        System.out.println(str);
+    }
+}
