941d3b5dd9d45ecea2c67b05c2f22d435aa4442f
Maia <maya.ceb.96@gmail.com>
2022-05-20 20:11:03 +0300

    8.1 Sum of numbers

--- Lesson 8/Class Exercise 8.1/src/Main.java
+++ Lesson 8/Class Exercise 8.1/src/Main.java
@@ -1,24 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     /*
 Implement a method sumInRange for calculating the sum of numbers in the range
 from (inclusive), to (exclusive).
 
 Sample Input 1: 1 2              Sample Output 1: 1
-Sample Input 2: 5 5              Sample Output 2: 5
+Sample Input 2: 5 5              Sample Output 2: 0
 Sample Input 3: 10 15            Sample Output 3: 60
       */
     public static void main(String[] args) {
-        System.out.println(sumInRange(1,2));
-        System.out.println(sumInRange(5, 5));
-        System.out.println(sumInRange(10, 15));
+        Scanner sc = new Scanner(System.in);
+         int from = sc.nextInt();
+         int to = sc.nextInt();
 
+         sumInRange(from, to);
     }
 
-    public static long sumInRange(int from, int to) {
-        long acc = from;
+    public static void sumInRange(int from, int to) {
+       int sum =0;
         for (int i = from + 1; i < to; i++) {
-            acc += i;
+            sum +=i;
         }
-        return acc;
+        System.out.println(sum);
     }
 }
