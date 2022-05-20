46779addf6ff8160ea30edfe46fa843dddae3348
Maia <maya.ceb.96@gmail.com>
2022-05-20 12:25:34 +0300

    4.Convert a date

--- Lesson 7/Exercise 7.4/src/Main.java
+++ Lesson 7/Exercise 7.4/src/Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Write a program that takes a date string formatted as YYYY-MM-DD as input, then
+        converts and outputs it as MM/DD/YYYY. For instance, the input 2007-07-21 will result in
+        the following output 07/21/2007.
+        Sample Input 1: 2012-09-28            Sample Output 1: 09/28/2012
+         */
         Scanner scanner = new Scanner(System.in);
         String initialDate = scanner.nextLine();
 
