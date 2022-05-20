c3b73837cf1992df5f29ea4d3c402a729f0982ad
Maia <maya.ceb.96@gmail.com>
2022-05-20 20:18:03 +0300

    8.1 Sum of numbers

--- Lesson 8/Class Exercise 8.1/src/Main.java
+++ Lesson 8/Class Exercise 8.1/src/Main.java
@@ -11,17 +11,18 @@ Sample Input 3: 10 15            Sample Output 3: 60
       */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         int from = sc.nextInt();
-         int to = sc.nextInt();
+        int from = sc.nextInt();
+        int to = sc.nextInt();
 
-         sumInRange(from, to);
+        System.out.println(sumInRange(from, to));
     }
 
-    public static void sumInRange(int from, int to) {
-       int sum =0;
-        for (int i = from + 1; i < to; i++) {
-            sum +=i;
+    public static int sumInRange(int from, int to) {
+        int sum = 0;
+        for (int i = from; i < to; i++) {
+            sum += i;
         }
-        System.out.println(sum);
+        return sum;
     }
 }
+
