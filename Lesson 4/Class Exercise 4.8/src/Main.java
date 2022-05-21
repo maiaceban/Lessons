da7c607b0c9952d584174588a7104d219c62031b
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:11:53 +0300

    Find X

--- Lesson 4/Class Exercise 4.8/src/Main.java
+++ Lesson 4/Class Exercise 4.8/src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+        Write a program that reads three double values a,b,c and then solving the simplest equation:
+        a * x + b = c. The program should output the value of X. It's guaranteed, a is not 0
+        Sample Input 1: 2 -1 2
+        Sample Output 1: 1.5
+
+         */
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        //a * x + b = c;
+        //a * x = c - b;
+        //x = (c - b) / a;
+        System.out.println((c - b) / a);
+    }
 }
