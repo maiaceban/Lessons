389a563cc1a018f86d1f578dac52e74bddf974a2
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:17:58 +0300

    First Java program

--- Lesson 1/Exercise 1.1/src/Main.java
+++ Lesson 1/Exercise 1.1/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    /**
+     * JAVA FUNDAMENTALS
+     */
     public static void main(String[] args) {
-        System.out.println("Hello Maia! ");
+        /*
+        Copy the given code to the IDE and put your name instead of <NAME> (e.g. Alex).
+        Use only English letters to write the name. Your solution should not contain <>.
+         */
+        System.out.println("Hello, my name is Maia!");
     }
 }
