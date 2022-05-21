b9f28fc573659f05796f86239d68e39e2c6b5015
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:23:01 +0300

    Reading integer numbers

--- Lesson 4/Exercise 4.1/src/Main.java
+++ Lesson 4/Exercise 4.1/src/Main.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that reads four integer numbers from one line and prints them each
+        in a new line. In the input line numbers are separated by one or more spaces.
+              Sample Input 1: 101 102 103 104
+              Sample Output 1:
+               101
+               102
+               103
+               104
+         */
         Scanner scanner = new Scanner(System.in);  // 101 102 103 104
 
         int a = scanner.nextInt();
