b1e5ce409a69a191b99d8d0864a17349455d6ef5
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:21:36 +0300

    We need to learn Java

--- Lesson 1/Exercise 1.4/src/Main.java
+++ Lesson 1/Exercise 1.4/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    /**
+     * Java Fundamentals
+     *
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that prints the following text:
+            WE NEED TO
+            LEARN JAVA
+            AS QUICKLY AS POSSIBLE
+        Hint: do not forget to print the empty lines!
+         */
         System.out.println("WE NEED TO");
         System.out.println();
         System.out.println("LEARN JAVA");
