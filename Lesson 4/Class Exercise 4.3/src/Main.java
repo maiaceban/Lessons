0fc965612dad803107bb81259e9852edcd008593
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:48:20 +0300

    Arithmetic operations

--- Lesson 4/Class Exercise 4.3/src/Main.java
+++ Lesson 4/Class Exercise 4.3/src/Main.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     * @param args
+     */
     public static void main(String[] args) {
+        /*
+        Write a program which reads a double value x and evaluates the result of
+                                         Math.pow(x, 3) + Math.pow(x, 2) + x + 1
+        Sample Input 1: 22.5
+        Sample Output 1: 11920.375
+         */
         Scanner scr = new Scanner(System.in);
         System.out.println("What is the value of x?: ");
         double x = scr.nextDouble();
