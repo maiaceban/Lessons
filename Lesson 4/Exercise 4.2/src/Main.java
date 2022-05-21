f8caca9fa04006e8f351381c853a9bcd554a5e7f
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:25:02 +0300

    How many nuts each squirrel will get

--- Lesson 4/Exercise 4.2/src/Main.java
+++ Lesson 4/Exercise 4.2/src/Main.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
+        /*
+        N squirrels found K nuts and decided to divide them equally. Determine how many
+        nuts each squirrel will get. So there are two positive numbers N and K, each of them is
+        not greater than 10000.
+            Sample Input 1:
+            3
+            14
+            Sample Output 1: 4
+         */
         Scanner sc = new Scanner(System.in);
         int s = sc.nextInt();   // Represent Squirrels
         int n = sc.nextInt();   // Represent Nuts
