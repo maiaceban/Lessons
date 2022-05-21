10b49da2eaa351254d6d4a14a6ed4e7288fb3844
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:43:09 +0300

    Check the given numbers are different

--- Lesson 4/Exercise 4.8/src/Main.java
+++ Lesson 4/Exercise 4.8/src/Main.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+
     public static void main(String[] args) {
+        /*
+        Write a program that reads three numbers and checks that they all are different, i.e.
+        not equal to one another. The output should be true or false.
+            Sample Input: 5 5 9
+            Sample Output: false
+         */
         Scanner scanner = new Scanner(System.in);
         int J = scanner.nextInt();
         int U = scanner.nextInt();
