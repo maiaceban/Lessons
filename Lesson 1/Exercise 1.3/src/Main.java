236605a157f02f7428e75298f11b49a4fb8b3393
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:19:43 +0300

    From 0 to 5

--- Lesson 1/Exercise 1.3/src/Main.java
+++ Lesson 1/Exercise 1.3/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    /**
+     * Java Fundamentals
+     * @param args
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that prints numbers from 0 to 5 inclusive.
+        Each number must be on a new line.
+         */
         System.out.println(0);
         System.out.println(1);
         System.out.println(2);
