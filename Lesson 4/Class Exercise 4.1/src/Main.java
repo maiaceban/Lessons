c252314ca96c390e01a10e18987c1340b4a9b879
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:36:30 +0300

    Scanning the input

--- Lesson 4/Class Exercise 4.1/src/Main.java
+++ Lesson 4/Class Exercise 4.1/src/Main.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Java Fundamentals
+     * @param args
+     */
     public static void main(String[] args) {
+        /*
+        Read a string (name) and an integer (age) from the console and output the text
+         */
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your first name?");
         String firstName = scanner.nextLine();
