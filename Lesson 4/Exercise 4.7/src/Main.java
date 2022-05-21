f1ce3cfc8acbf570f928457d001b018526711872
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:41:02 +0300

    Check the value

--- Lesson 4/Exercise 4.7/src/Main.java
+++ Lesson 4/Exercise 4.7/src/Main.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that reads an integer value and checks if it is less than 10 and
+        greater than 0. Your program should print a boolean value (true or false).
+            Sample Input: 0
+            Sample Output: false
+         */
         Scanner sc = new Scanner(System.in);
 
         int i = sc.nextInt();
