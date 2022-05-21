d9caf95ff1cf1f17613f45bcc36bd97ef02549c1
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:54:56 +0300

    non-negative integer

--- Lesson 4/Class Exercise 4.5/src/Main.java
+++ Lesson 4/Class Exercise 4.5/src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     * @param args
+     */
+    public static void main(String[] args) {
+        /* Write a program that finds the number of tens in a non-negative integer N(0<N<100000).
+	        If N is 264, the number of tens is 6
+	        If N is 4136, the number of tens is 3
+	        If N is 101, the number of tens is 0 */
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println((num % 100) / 10);
+
+    }
 }
