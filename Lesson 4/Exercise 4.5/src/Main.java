29daa5c9ba0dc122ccc4a479027ea8273d991f98
Maia <maya.ceb.96@gmail.com>
2022-05-21 20:31:12 +0300

    Distance

--- Lesson 4/Exercise 4.5/src/Main.java
+++ Lesson 4/Exercise 4.5/src/Main.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that reads the distance between the two cities in km and the travel
+        time by bus in hours, and outputs the average speed of the bus.
+            Sample Input 1:
+            100
+            2
+            Sample Output 1: 50.0
+         */
         Scanner sc = new Scanner(System.in);
 
         float distance = sc.nextFloat();
