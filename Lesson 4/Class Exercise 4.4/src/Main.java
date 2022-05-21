5881e1741c0cfbc39e96acdfebb57d7893fa2fa6
Maia <maya.ceb.96@gmail.com>
2022-05-21 19:52:35 +0300

    Relational operators

--- Lesson 4/Class Exercise 4.4/src/Main.java
+++ Lesson 4/Class Exercise 4.4/src/Main.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     * @param args
+     */
     public static void main(String[] args) {
+        /*
+        Write a program that reads the number a,b,c and checks if any pair of them sums to 20.
+        The program must output true or false
+        Sample Input 1: 1 2 3          Simple Output 1: false
+        Sample Input 2: 4 16 7         Simple Output 2: true
+         */
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
