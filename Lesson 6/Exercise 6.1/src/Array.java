45ec51119620de6117e783e555c2c2ec6c7ab34f
Maia <maya.ceb.96@gmail.com>
2022-05-22 17:52:26 +0300

    Initializing an array of characters

--- Lesson 6/Exercise 6.1/src/Array.java
+++ Lesson 6/Exercise 6.1/src/Array.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 
 public class Array {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
         char[] characters = {'a', 'z', 'e', 'd'};
 
