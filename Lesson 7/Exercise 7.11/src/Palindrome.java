25f97b75d05d54c51393d14270b61620f5875347
Maia <maya.ceb.96@gmail.com>
2022-05-22 18:32:19 +0300

    Palindrome

---
+++ Lesson 7/Exercise 7.11/src/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+    public static void main(String[] args) {
+        /*
+         * Write a program that reads a string and checks whether it is a palindrome, i.e. it
+         * reads the same both left-to-right and right-to-left.
+         * The program must output “yes” if the string is a palindrome and “no” otherwise.
+         * Sample Input: kayak
+         * Sample Output: yes
+         */
+
+
+
+    }
+}
