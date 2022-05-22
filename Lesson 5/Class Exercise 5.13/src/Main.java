90d7fad7045390fa41d6013815ef907a3827f6f1
Maia <maya.ceb.96@gmail.com>
2022-05-22 13:06:26 +0300

    Maximum element divisible by four

--- Lesson 5/Class Exercise 5.13/src/Main.java
+++ Lesson 5/Class Exercise 5.13/src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nrOfElements = scanner.nextInt();
+        long max = 0;
+
+        for (int i = 0; i < nrOfElements; i++) {
+            int nr = scanner.nextInt();
+            if (nr % 4 == 0 && nr > max) {
+                max = nr;
+            }
+        }
+
+        System.out.println(max);
+    }
 }
+//12 16 87 58 25 73 86 36 79 40 12 89 32
+//40
