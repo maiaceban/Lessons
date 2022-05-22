f508ce684dfe1690aa60edbe1c39f30dbc6116ea
Maia <maya.ceb.96@gmail.com>
2022-05-22 17:53:29 +0300

    Initializing an array of integers

--- Lesson 6/Exercise 6.2/src/Main.java
+++ Lesson 6/Exercise 6.2/src/Main.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
-
+/**
+ * Tekwill Academy
+ * Java Fundamentals
+ */
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {12, 17, 8, 101, 33}; // initialize the array
