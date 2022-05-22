beb80255dc3b1f3fccef183f77d241593b759f7f
Maia <maya.ceb.96@gmail.com>
2022-05-22 17:49:29 +0300

    Initializing an array of longs

--- Lesson 6/Class Exercise 6.1/src/Main.java
+++ Lesson 6/Class Exercise 6.1/src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
         long[] longNumbers = new long[]{100000000001L, 1000000000002L, 100000000003L};
 
