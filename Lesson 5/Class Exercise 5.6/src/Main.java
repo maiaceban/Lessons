28d930235a1837777dccc3f4a6f2cbb315efdf40
Maia <maya.ceb.96@gmail.com>
2022-05-22 12:49:10 +0300

    The integer barrier (Branching statement)

--- Lesson 5/Class Exercise 5.6/src/Main.java
+++ Lesson 5/Class Exercise 5.6/src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();   // declaring a variable and initialize it with user input
