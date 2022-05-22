3719e2b91be557e76853564640446eda04d2ed5b
Maia <maya.ceb.96@gmail.com>
2022-05-22 18:09:52 +0300

    Number of occurrences

--- Lesson 7/Exercise 7.5/src/Main.java
+++ Lesson 7/Exercise 7.5/src/Main.java
@@ -3,7 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * Tekwill Academy
+ * Java Fundamentals
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         /*
