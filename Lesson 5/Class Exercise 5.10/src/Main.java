59533fdf96380c94c1f887548b33d026e7968d6e
Maia <maya.ceb.96@gmail.com>
2022-05-22 12:57:24 +0300

    A simple calculator (Control Flow)

--- Lesson 5/Class Exercise 5.10/src/Main.java
+++ Lesson 5/Class Exercise 5.10/src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long nr1 = scanner.nextLong();
