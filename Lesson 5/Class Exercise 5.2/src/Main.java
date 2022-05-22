c650ba1288900ba6ac16b8ec601d9c0808a4c21c
Maia <maya.ceb.96@gmail.com>
2022-05-22 12:34:47 +0300

    Number is positive with Ternary

--- Lesson 5/Class Exercise 5.2/src/Main.java
+++ Lesson 5/Class Exercise 5.2/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int number = scanner.nextInt();
         String result = number > 0 ? "YES" : "NO";
         System.out.println(result);
