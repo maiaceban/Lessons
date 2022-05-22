7bf2991457c57dbc0bcd4c80e19ac6d9aba9eb46
Maia <maya.ceb.96@gmail.com>
2022-05-22 13:00:40 +0300

    Numbers divisible by six (Control Flow)

--- Lesson 5/Class Exercise 5.11/src/Main.java
+++ Lesson 5/Class Exercise 5.11/src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nr = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < nr; i++) {
+            int temp = scanner.nextInt();
+            if (temp % 6 == 0) {
+                sum += temp;
+            }
+        }
+
+        System.out.println(sum);
+        // Input: 8 11 12 68 6 98 81 36 86
+        // Output: 54
+    }
 }
