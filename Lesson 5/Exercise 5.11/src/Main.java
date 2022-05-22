ca9e8aa92b4901f9b342e4447630117bc3607ae3
Maia <maya.ceb.96@gmail.com>
2022-05-22 12:21:28 +0300

    Hello, Java platform

---
+++ Lesson 5/Exercise 5.11/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+    public static void main(String[] args) {
+        // Add comments to the code so the program outputs "Hello, Java platform".
+        System.out.print("Hello, ");
+//        System.out.print("world");
+//        System.out.print(" and ");
+        System.out.print("Java");
+        System.out.print(" platform!");
+    }
+}
