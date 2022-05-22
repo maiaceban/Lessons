dad44fb073317795870902f6ef8dff2fda5d96f0
Maia <maya.ceb.96@gmail.com>
2022-05-22 12:51:13 +0300

    Leap year (Control Flow)

--- Lesson 5/Class Exercise 5.7/src/Main.java
+++ Lesson 5/Class Exercise 5.7/src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
