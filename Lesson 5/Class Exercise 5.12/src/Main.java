e8f50b97224a076fdee2ad0cf5515f21b620e667
Maia <maya.ceb.96@gmail.com>
2022-05-22 13:03:23 +0300

    The sum of elements

--- Lesson 5/Class Exercise 5.12/src/Main.java
+++ Lesson 5/Class Exercise 5.12/src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int i = scanner.nextInt();
+            if (i == 0) break;
+            sum += i;
+        }
+        System.out.println(sum);
+    }
 }
+//3 6 8 0
+//17
