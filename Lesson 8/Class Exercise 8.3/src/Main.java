8cd909e8de62bfccac0a13d6adbe355da85f54ae
Maia <maya.ceb.96@gmail.com>
2022-05-23 19:23:09 +0300

    Get first and last elements

---
+++ Lesson 8/Class Exercise 8.3/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    // write a method here
+    public static int[] getFirstAndLast(int [] arr) {
+        int a = arr.length - 1;
+        return new int[] {arr[0], arr[a]};
+    }
+        /* Do not change code below */
+        public static void main (String[] args){
+            Scanner scanner = new Scanner(System.in);
+            int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] result = getFirstAndLast(array);
+            Arrays.stream(result).forEach(e -> System.out.print(e + " "));
+        }
+
+    }
