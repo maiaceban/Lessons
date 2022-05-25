98cc74dea1dea70e90f03ab4c6a95f9eb16bf9d1
Maia <maya.ceb.96@gmail.com>
2022-05-25 11:38:35 +0300

    Books (Defining classes)

---
+++ Lesson 7/Exercise 7.12/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    /**
+     * Tekwill Academy
+     * Java Fundamentals
+     */
+
+    public static void main(String[] args) {
+        /*
+        Write a program that reads a string and outputs "true" only when the letters of this
+        string form a substring of the ordered English alphabet, for example, "abc", "xy", "pqrst".
+        Otherwise, it should print out "false".
+        Note: string can consist of a single character. Assume an empty string as an alphabet substring.
+            Sample Input 1: abc Sample Output 1: true
+            Sample Input 2: bce Sample Output 2: false
+         */
+
+    }
+}
