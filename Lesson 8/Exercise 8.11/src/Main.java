bbb4b42b4e2ffeac13fb84fd1224666f01230dfd
Maia <maya.ceb.96@gmail.com>
2022-05-25 14:24:39 +0300

    N-th power

---
+++ Lesson 8/Exercise 8.11/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    /*
+    It is possible to find a n-th power much quicker than by making n multiplications!
+    To do this you need to use the following recurrence relations: ....
+    Implement the algorithm of quick exponentiation using a recursion method.
+        Sample Input 1:                 Sample Output 1: 2.0
+        2.0
+        1
+        Sample Input 2:                 Sample Output 2: 57.665
+        1.5
+        10
+
+     */
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+
+}
