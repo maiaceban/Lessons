5c9d217173bf52a4374f35d0889dc64795fe972a
Maia <maya.ceb.96@gmail.com>
2022-05-25 14:46:15 +0300

    Inner boxes (Define a class)

---
+++ Lesson 9/Exercise 9.3/src/Box.java
@@ -0,0 +1,13 @@
+public class Box {
+    /*
+    Define a class named Box. It must have four fields:
+    ● length, height and width of the double type
+    ● a field named innerBox of the same type as the defined class
+     */
+    public static void main(String[] args) {
+        double length;
+        double height;
+        double width;
+        boolean innerBox;
+    }
+}
