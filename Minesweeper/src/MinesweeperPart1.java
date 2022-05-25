1e57884ae7f53f5ead6c4095aa427633980167f3
Maia <maya.ceb.96@gmail.com>
2022-05-25 17:30:40 +0300

    Stage 2/5: Flexible mines

--- Minesweeper/src/MinesweeperPart1.java
+++ Minesweeper/src/MinesweeperPart1.java
@@ -1,16 +1,4 @@
 public class MinesweeperPart1 {
-    /*
-Description
-Minesweeper is a game with a field in which there are a lot of mines. You need to find where all these mines are located.
-To help you find the mines, there are cells that show how many mines are located around them.
-Your first step would be easy. In this step, you need to output some state of the field.
-The field contains mines marked as 'X' and safe places marked as '.', but you can choose your own symbols for marking
-places with mines and safe places. Note that all the mines are in plain sight, so you don't have to hide them from
-the user at this stage!
-Example
-In the example, there are 10 mines in a 9x9 field. You can write to the output of any
-other configuration of mines.
-     */
     public static void main(String[] args) {
         System.out.println(".X....X..");
         System.out.println(".....X...");
