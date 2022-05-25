176994036da037a620cf39b22540479cb2c82a24
Maia <maya.ceb.96@gmail.com>
2022-05-25 15:25:05 +0300

    Complex numbers

---
+++ Lesson 9/Exercise 9.5/src/Complex.java
@@ -0,0 +1,15 @@
+public class Complex {
+    /*
+    Write a class named Complex. It must have two double fields real, imaginary and a
+    constructor to initialize the fields.
+     */
+    double real;
+    double imaginary;
+
+    public static void main(String[] args) {
+
+        Complex complex = new Complex();
+        System.out.println(complex.real);
+        System.out.println(complex.imaginary);
+    }
+}
