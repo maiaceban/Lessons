5e703aff0d311eb1c0e263055501b6227843acae
Maia <maya.ceb.96@gmail.com>
2022-05-25 15:25:47 +0300

    Binary search (not finished)

---
+++ Lesson 9/Exercise 9.4/src/Arrays.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Arrays {
+    /*
+     The class Arrays includes a lot of useful methods for processing arrays. One of them is the binarySearch method.
+     It allows you to quickly find the index of an element in the given ordered array.
+     You just need to pass the given parameters in the same order to this method.
+     In the main class get an array of 10 integers with a Scanner and one more integer to find the position of.
+     Use binarySearch method to find its position in the array. Don’t use imports in this exercise.
+        Sample Input 1:             Sample Output 1:
+        0 1 2 3 4 5 6 7 8 9         5
+        5
+     */
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] intArray = new int[] { 0,1,2,3,4,5,6,7,8,9};
+
+
+
+
+    }
+}
