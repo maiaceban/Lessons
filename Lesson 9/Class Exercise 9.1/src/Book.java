72283f59d8e1a82ba1295e08eccedaaa31f718b3
Maia <maya.ceb.96@gmail.com>
2022-05-25 14:41:40 +0300

    Books (Defining classes)

--- Lesson 9/Class Exercise 9.1/src/Book.java
+++ Lesson 9/Class Exercise 9.1/src/Book.java
@@ -1,12 +1,7 @@
 public class Book {
     public static void main(String[] args) {
-        Book b1 = new Book();
-        String title = "Harry Potter";
-        int yearOfPublishing = 1998;
-        boolean isAvailable = true;
-
-        System.out.println(title);
-        System.out.println(yearOfPublishing);
-        System.out.println(isAvailable);
+        String title;
+        int yearOfPublishing;
+        boolean isAvailable;
     }
 }
