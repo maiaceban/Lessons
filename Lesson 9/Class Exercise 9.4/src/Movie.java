bb31bb6fe952cab30346c3d7af9de1743c7d4b13
Maia <maya.ceb.96@gmail.com>
2022-05-26 12:44:13 +0300

    not finished

--- Lesson 9/Class Exercise 9.4/src/Movie.java
+++ Lesson 9/Class Exercise 9.4/src/Movie.java
@@ -3,16 +3,20 @@ public class Movie {
     String desc;
     int year;
 
+    public Movie(){
+
+    }
+
     public Movie(String title, String desc, int year) {
         this.title = title;
-        this.desc = desc;
         this.year = year;
+        this.desc = "empty";
     }
 
     public Movie(String title, int year) {
         this.title = title;
+        this.desc = desc;
         this.year = year;
-        this.desc = "empty";
 
     }
 }
