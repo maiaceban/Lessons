c6c12ae4170fa101130a7023eba6ea2a9ea706b0
Maia <maya.ceb.96@gmail.com>
2022-05-27 18:57:27 +0300

    N-th power

--- Lesson 8/Exercise 8.11/src/Main.java
+++ Lesson 8/Exercise 8.11/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Long.sum;
+
 public class Main {
     /*
     It is possible to find a n-th power much quicker than by making n multiplications!
@@ -11,9 +13,24 @@ public class Main {
         Sample Input 2:                 Sample Output 2: 57.665
         1.5
         10
-
      */
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
+
+    public static double power(double a, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            return power(a * a, n / 2);
+        } else {
+            return a * power(a, n - 1);
+        }
+    }
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double power = scanner.nextDouble();
+        System.out.println(power);
+    }
+
 
 }
