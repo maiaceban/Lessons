be29281ef51de055fa71f39915366bd8c227dab4
Maia <maya.ceb.96@gmail.com>
2022-05-29 20:36:12 +0300

    Too many cats (not finished)

---
+++ Lesson 10/Exercise 10.7/src/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    /*
+    Let's count the cats! Implement a class named Cat and a static method
+getNumberOfCats.
+     */
+
+    // write static and instance variables
+
+    public Cat(String name, int age) {
+// implement the constructor
+
+// do not forget to check the number of cats
+
+    }
+    public static int getNumberOfCats() {
+// implement the static method
+
+        return 0;
+    }
+}
