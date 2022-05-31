76ae47638c265b8cfac71bc59eabc0822021bec9
Maia <maya.ceb.96@gmail.com>
2022-05-31 19:37:36 +0300

    tic-tak-toe

--- TicTacToeGame/src/tictactoe/Main.java
+++ TicTacToeGame/src/tictactoe/Main.java
@@ -1,29 +1,7 @@
 package tictactoe;
+/*
 
-import java.util.Scanner;
-
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useDelimiter("");
-        char[][] state = new char[3][3];
-        int i = 0;
-        int j = 0;
-        int size = state.length - 1;
-        System.out.print("Enter cells: ");
-        while (scanner.hasNext()) {
-            state[i][j] = scanner.next().charAt(0);
-            if (j < size) {
-                j++;
-            } else if (i < size) {
-                j = 0;
-                i++;
-            } else {
-                break;
-            }
 
-            TikTakToeGame game = new TikTakToeGame(state);
-            System.out.println(game.getState());
-            System.out.println(game.getStatus());
-        }
-    }
 }
