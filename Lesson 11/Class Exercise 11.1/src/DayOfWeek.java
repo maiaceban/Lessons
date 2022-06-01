2841023d6917ed20a70d135287686f2f59e124aa
Maia <maya.ceb.96@gmail.com>
2022-06-01 20:05:03 +0300

    Enum ex

--- Lesson 11/Class Exercise 11.1/src/DayOfWeek.java
+++ Lesson 11/Class Exercise 11.1/src/DayOfWeek.java
@@ -1,9 +1,5 @@
 public enum DayOfWeek {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
 
-    public static void main(String[] args) {
-        for (DayOfWeek day : DayOfWeek.values())
-            System.out.println(day);
-    }
 }
 
