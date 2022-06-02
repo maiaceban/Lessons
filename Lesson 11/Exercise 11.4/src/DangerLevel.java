29b3b9a99fe641c85ee9b02c91e469d06a462784
Maia <maya.ceb.96@gmail.com>
2022-06-02 23:13:20 +0300

    Currencies

---
+++ Lesson 11/Exercise 11.4/src/DangerLevel.java
@@ -0,0 +1,14 @@
+public enum DangerLevel {
+    HIGH(3),
+    MEDIUM(2),
+    LOW(1);
+
+   final int level;
+    DangerLevel(int danger){
+        this.level = danger;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+}
