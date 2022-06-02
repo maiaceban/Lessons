d8821dd19155fc329b694f374cce120a8f37ffc1
Maia <maya.ceb.96@gmail.com>
2022-06-02 22:40:58 +0300

    Primitive (not finished)

---
+++ Lesson 11/Exercise 11.1/src/Primitive.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Primitive {
+
+    public static boolean toPrimitive(Boolean b) {
+        if (b == null) {
+            return false;
+        } else if (b != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean b = scanner.hasNextBoolean();
+        System.out.println(toPrimitive(b));
+    }
+}
