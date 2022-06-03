02d7bb8d3b8f7c2fff6914b81699547f28adf3b4
Maia <maya.ceb.96@gmail.com>
2022-06-03 19:41:09 +0300

    Fix the non-compiled hierarchy

--- Lesson 12/Class Exercise 12.1/src/Main.java
+++ Lesson 12/Class Exercise 12.1/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Maia", "02/05/1996");
-        System.out.println(employee.birthDate);
-        Employee regularEmployee = new RegularEmployee("Maia", "02/05/1996", 9500L, "02.12.2020");
-        System.out.println(regularEmployee.name);
-        Employee contractEmployee = new ContractEmployee("Maia", "02/05/1996", 8L, "nedeterminat");
-        System.out.println(contractEmployee.name);
+        System.out.println("Name: " + employee.name);
+        RegularEmployee re = new RegularEmployee("Maia", "02/05/1996", 9500L, "02.12.2020");
+        System.out.println("The salary is: " + re.salary);
+        ContractEmployee ce = new ContractEmployee("Maia", "02/05/1996", 350L, "nedeterminat");
+        System.out.println("Pay Per Hours: " + ce.payPerHour);
     }
 }
