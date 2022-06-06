13378f957cddfdda90034140a3351663c03b3e61
Maia <maya.ceb.96@gmail.com>
2022-06-06 21:44:38 +0300

    Time as a String (toString)

---
+++ Lesson 13/Class Exercise 13.1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Time time1 = new Time(23, 59, 59);
+        Time time2 = new Time(11, 8, 5);
+        Time time3 = new Time(1, 1, 1);
+        System.out.println(time1);
+        System.out.println(time2);
+        System.out.println(time3);
+    }
+}
