46c6e6e969353f57e32ed2823ad3c2ce5a5194dd
Maia <maya.ceb.96@gmail.com>
2022-06-17 15:40:44 +0300

    String Reverser

--- Lesson 14/Class Exercise 14.3/src/Main.java
+++ Lesson 14/Class Exercise 14.3/src/Main.java
@@ -4,12 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        StringReverser reverser = new StringReverser() {
-            @Override
-            public String reverse(String str) {
-                return new StringBuilder(str).reverse().toString();
-            }
-        };
+        StringReverser reverser = str -> new StringBuilder(str).reverse().toString();
         System.out.println(reverser.reverse(line));
 
     }
