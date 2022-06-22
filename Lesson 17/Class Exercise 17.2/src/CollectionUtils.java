e2809eca9e25bb91f94ada21c644fe3e1f966162
Maia <maya.ceb.96@gmail.com>
2022-06-22 19:57:20 +0300

    Blacklist (filterPhones method)

--- Lesson 17/Class Exercise 17.2/src/CollectionUtils.java
+++ Lesson 17/Class Exercise 17.2/src/CollectionUtils.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class CollectionUtils {
     public static  Collection<String> filterPhones(Collection<String> phones, Collection<String> blacklist){
+//       phones.removeAll(blacklist);
         for (String phoneNumber : blacklist){
             if (phones.contains(phoneNumber)){
                 phones.remove(phoneNumber);
