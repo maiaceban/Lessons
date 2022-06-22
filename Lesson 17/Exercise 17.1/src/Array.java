89a15f6a2c7326e42e17e9809584e7dfab7a117a
Maia <maya.ceb.96@gmail.com>
2022-06-22 22:44:26 +0300

    Output the list's elements (ArrayList)

--- Lesson 17/Exercise 17.1/src/Array.java
+++ Lesson 17/Exercise 17.1/src/Array.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Array {
     public static void main(String[] args) {
@@ -8,15 +9,19 @@ public class Array {
         new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
     Output each of its elements in the loop. Each element must be in a new line.
      */
-        ArrayList<String> nameList = new ArrayList<>();
+//        ArrayList<String> nameList = new ArrayList<>();
+//
+//        nameList.add("Mr.Green");
+//        nameList.add("Mr.Yellow");
+//        nameList.add("Mr.Red");
+//        System.out.println(nameList); //[Mr.Green, Mr.Yellow, Mr.Red]
+//        System.out.println(nameList.get(0)); //Mr.Green
+//        System.out.println(nameList.get(1)); //Mr.Yellow
+//        System.out.println(nameList.get(2)); //Mr.Red
 
-        nameList.add("Mr.Green");
-        nameList.add("Mr.Yellow");
-        nameList.add("Mr.Red");
-        System.out.println(nameList); //[Mr.Green, Mr.Yellow, Mr.Red]
-        System.out.println(nameList.get(0)); //Mr.Green
-        System.out.println(nameList.get(1)); //Mr.Yellow
-        System.out.println(nameList.get(2)); //Mr.Red
-
+        ArrayList<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        for (String name : nameList){
+            System.out.println(name);
+        }
     }
 }
