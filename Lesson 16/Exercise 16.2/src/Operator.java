d2214da1db748b524c53ad9d642cca65c3a07821
Maia <maya.ceb.96@gmail.com>
2022-06-23 22:02:18 +0300

    Expression closure (Lambda expressions)

--- Lesson 16/Exercise 16.2/src/Operator.java
+++ Lesson 16/Exercise 16.2/src/Operator.java
@@ -12,35 +12,4 @@ public class Operator {
     public static int c = 30;
     public static DoubleUnaryOperator unaryOperator = x -> a * x * x + b * x + c;
 
-    public static int getA() {
-        return a;
-    }
-
-    public static void setA(int a) {
-        Operator.a = a;
-    }
-
-    public static int getB() {
-        return b;
-    }
-
-    public static void setB(int b) {
-        Operator.b = b;
-    }
-
-    public static int getC() {
-        return c;
-    }
-
-    public static void setC(int c) {
-        Operator.c = c;
-    }
-
-    public static DoubleUnaryOperator getUnaryOperator() {
-        return unaryOperator;
-    }
-
-    public static void setUnaryOperator(DoubleUnaryOperator unaryOperator) {
-        Operator.unaryOperator = unaryOperator;
-    }
 }
