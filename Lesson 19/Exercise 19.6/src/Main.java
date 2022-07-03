c3da998a55d629adb015a83d68f6b45f3db7ffd3
Maia <maya.ceb.96@gmail.com>
2022-07-03 14:20:49 +0300

    Converting and multiplying (Exception handling)

--- Lesson 19/Exercise 19.6/src/Main.java
+++ Lesson 19/Exercise 19.6/src/Main.java
@@ -8,7 +8,6 @@ Your task is to write a program that reads a sequence of strings and converts th
 to integer numbers, multiplying by 10. Some input strings may have an invalid format,
 for instance: "abc". The sequence’s length may be different. Its ending is indicated by
 “0”. Zero serves as a stop character and should not be included in the sequence.
-
 If a string can be converted to an integer number, output the number multiplied by
 10. Otherwise, output the string "Invalid user input: X" where X is the input string. To
 better understand see examples below.
