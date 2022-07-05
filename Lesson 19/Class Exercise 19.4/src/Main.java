3aaf61b0529313b0fe166b6d81ee63e12ab5dda4
Maia <maya.ceb.96@gmail.com>
2022-07-05 19:15:45 +0300

    Throw an IOException

---
+++ Lesson 19/Class Exercise 19.4/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+/**
+ * Throw an IOException
+ */
+public class Main {
+
+    //change this method
+    public static void method() throws IOException {
+        throw new IOException();
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+        }
+    }
+}
