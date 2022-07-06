d959e8244027536362ce17b6dd1717462deeedc6
Maia <maya.ceb.96@gmail.com>
2022-07-06 21:34:00 +0300

    Movable (not finished) prima incercare

--- Movable/src/Program.java
+++ Movable/src/Program.java
@@ -1,6 +1,7 @@
 public class Program {
     public static void main(String[] args) {
-        MovablePoint p=new MovablePoint(1,1,1,1);
-        MovableCircle c=new MovableCircle(p,5);
+        MovablePoint p = new MovablePoint(1, 1, 1, 1);
+        MovableCircle c = new MovableCircle(p, 5);
+
     }
 }
