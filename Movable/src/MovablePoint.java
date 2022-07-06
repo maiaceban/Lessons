2e9f840ce08f7dedba88f08a0fd5560eb3b2c627
Maia <maya.ceb.96@gmail.com>
2022-07-06 21:27:45 +0300

    Movable (not finished) prima incercare

--- Movable/src/MovablePoint.java
+++ Movable/src/MovablePoint.java
@@ -1,40 +1,7 @@
 public class MovablePoint {
-    //    public int x;
-//    public int y;
+
     public int xSpeed;
     public int ySpeed;
-    //    public MovablePoint(int x, int y, int xSpeed, int ySpeed)
-//    {
-//        this.x=x;
-//        this.y=y;
-//        this.xSpeed=xSpeed;
-//        this.ySpeed=ySpeed;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "MovablePoint{" + "x=" + x + ", y=" + y + ", xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + '}';
-//    }
-//
-//    @Override
-//    public void moveUp() {
-//        y+=ySpeed;
-//    }
-//
-//    @Override
-//    public void moveDown() {
-//        y-=ySpeed;
-//    }
-//
-//    @Override
-//    public void moveRight() {
-//        x+=xSpeed;
-//    }
-//
-//    @Override
-//    public void moveLeft() {
-//        x-=xSpeed;
-//    }
     int x;
     int y;
 
